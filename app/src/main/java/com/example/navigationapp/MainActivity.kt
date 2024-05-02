4893599ccfb3c517b87bf19792a01e8d92b0d269
tanjidul <369tomu@gmail.com>
2024-05-02 13:30:58 +0600

    navigation added

---
+++ app/src/main/java/com/example/navigationapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.navigationapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.navigationapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        var navController = findNavController(R.id.fragmentContainerView)
+        binding.bottomBar.setupWithNavController(navController)
+
+    }
+}
